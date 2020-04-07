88f1cab08ede57e0b3944df457656855cec82462
黄雪松_前端 <626732786@qq.com>
2020-04-07 10:22:54 +0800

    reverse()

---
+++ 344. 反转字符串/1.js
@@ -0,0 +1,12 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ * @date 2020-04-07 10:22:37
+ */
+var reverseString = function (s) {
+    s.reverse();
+    return s
+};
+
+// 执行用时 :132 ms, 在所有 JavaScript 提交中击败了75.12%的用户
+// 内存消耗 :46.7 MB, 在所有 JavaScript 提交中击败了94.80%的用户
