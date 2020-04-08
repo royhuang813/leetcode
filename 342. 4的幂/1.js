29d5472d8edff62a30d068c3f00c8315cb951e27
黄雪松_前端 <626732786@qq.com>
2020-04-08 09:51:50 +0800

    循环

---
+++ 342. 4的幂/1.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ * @date 2020-04-08 09:51:34
+ */
+var isPowerOfFour = function (num) {
+    while (true) {
+        if (num === 1) {
+            return true;
+        }
+        if (num % 4 !== 0 || num === 0) {
+            return false;
+        }
+        num /= 4;
+    }
+};
+
+// 执行用时 :104 ms, 在所有 JavaScript 提交中击败了14.52%的用户
+// 内存消耗 :35.5 MB, 在所有 JavaScript 提交中击败了80.00%的用户
